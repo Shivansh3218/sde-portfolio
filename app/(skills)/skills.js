1362b8d3ff37023efd27e7aaf30a2e158689870c
komalahire <komala@navgurukul.org>
2024-02-29 22:42:22 +0530

    created skill section

--- app/(skills)/skills.js
+++ app/(skills)/skills.js
@@ -1,56 +1,87 @@
 "use client";
-
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image"; // Import Image from nextjs
 import "./style.css"; // Importing the CSS file
 
 function Skill() {
-  useEffect(() => {
-    // const copy = document.querySelector(".logos-slide").cloneNode(true);
-    // document.querySelector(".logos").appendChild(copy);
-
-    // Cleanup function
-    return () => {
-      // Remove the appended copy when component unmounts
-    //   document.querySelector(".logos").removeChild(copy);
-    };
-  }, []); // Empty dependency array ensures useEffect runs only once after initial render
-
   return (
     <>
-        <div className="logos">
-      <div className="logos-slide">
-        <Image width={150} height={137} src="/3m.svg" alt="3M" />
-        <Image width={150} height={137} src="/barstool-store.svg" alt="Barstool Store" />
-        <Image width={150} height={137} src="/budweiser.svg" alt="Budweiser" />
-        <Image width={150} height={137} src="/buzzfeed.svg" alt="BuzzFeed" />
-        <Image width={150} height={137} src="/forbes.svg" alt="Forbes" />
-        <Image width={150} height={137} src="/macys.svg" alt="Macys" />
-        <Image width={150} height={137} src="/menshealth.svg" alt="Mens Health" />
-        <Image width={150} height={137} src="/mrbeast.svg" alt="Mr. Beast" />
-        <Image width={150} height={137} src="/forbes.svg" alt="Forbes" />
-        <Image width={150} height={137} src="/macys.svg" alt="Macys" />
-        <Image width={150} height={137} src="/menshealth.svg" alt="Mens Health" />
-        <Image width={150} height={137} src="/mrbeast.svg" alt="Mr. Beast" />
+      <div className="wrapper">
+        <div className="slider">
+          <div className="slide-track">
+            <div className="slide">
+              <Image src="/logos/css.png" alt="Batman" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/docker.webp" alt="Spiderman" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/express.png" alt="Black Adam" width={500} height={300} />
+            </div>
+
+             <div className="slide">
+              <Image src="/logos/figma.png" alt="Iron Man" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/Firebase.png" alt="SuperMan" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/gitwhite.png" alt="Captain America" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/html.png" alt="Black Panther" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/js.png" alt="Aquaman" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/mui.png" alt="Hulk" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/mysql.png" alt="Flash" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/next.png" alt="Thor" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/node-js.png" alt="Ant-Man" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/postger.png" alt="Wonder Woman" width={500} height={300} />
+            </div>
+
+            <div className="slide">
+              <Image src="/logos/react.png" alt="Green Lantern" width={500} height={300} />
+            </div> 
+            <div className="slide">
+              <Image src="/logos/ReactNative.png" alt="Green Lantern" width={500} height={300} />
+            </div> 
+            <div className="slide">
+              <Image src="/logos/Reactquery.png" alt="Green Lantern" width={500} height={300} />
+            </div> 
+            <div className="slide">
+              <Image src="/logos/redux.png" alt="Green Lantern" width={500} height={300} />
+            </div> 
+            <div className="slide">
+              <Image src="/logos/tailwind.png" alt="Green Lantern" width={500} height={300} />
+            </div> 
+            <div className="slide">
+              <Image src="/logos/ts.png" alt="Green Lantern" width={500} height={300} />
+            </div> 
+          </div>
+        </div>
       </div>
-    </div>
-    
-    {/* <div className="logos2">
-      <div className="logos-slide2">
-        <Image width={150} height={137} src="/3m.svg" alt="3M" />
-        <Image width={150} height={137} src="/barstool-store.svg" alt="Barstool Store" />
-        <Image width={150} height={137} src="/budweiser.svg" alt="Budweiser" />
-        <Image width={150} height={137} src="/buzzfeed.svg" alt="BuzzFeed" />
-        <Image width={150} height={137} src="/forbes.svg" alt="Forbes" />
-        <Image width={150} height={137} src="/macys.svg" alt="Macys" />
-        <Image width={150} height={137} src="/menshealth.svg" alt="Mens Health" />
-        <Image width={150} height={137} src="/mrbeast.svg" alt="Mr. Beast" />
-        <Image width={150} height={137} src="/forbes.svg" alt="Forbes" />
-        <Image width={150} height={137} src="/macys.svg" alt="Macys" />
-        <Image width={150} height={137} src="/menshealth.svg" alt="Mens Health" />
-        <Image width={150} height={137} src="/mrbeast.svg" alt="Mr. Beast" />
-      </div>
-    </div> */}
     </>
   );
 }
